9a10368701168081ba870823ffad03244f205455
fritzvd <fritz.vandeventer@nelen-schuurmans.nl>
2016-08-01 14:57:48 +0200

    add release and asset script

--- release-asset.js
+++ release-asset.js
@@ -19,8 +19,13 @@ zip.batchAdd(files, function () {
   zip.writeToFileSycn(fileName);
 });
 
-function uploadAssets (ghrelease) {
+function uploadAssets (err, body) {
+  if (err) { throw err }
+  console.log('Created release, getting ready to upload assets')
+  var ghrelease = client.release(pkg.repository.name, body.id);
   var archive = fs.readFileSync(fileName);
+
+  var html_url = body.html_url;
 
   ghrelease.uploadAssets(archive, {
     contentType: 'application/zip',
@@ -34,21 +39,29 @@ function uploadAssets (ghrelease) {
       var message = [requesterr.message].concat(bodyerrors);
       throw new Error(message.join('\n'));
     }
-    // if (err) { throw err; }
-    // rimraf('./tmp', function (rerr) {
-    //   if (rerr) { throw rerr; }
-    //   console.log('succesfully cleaned up tmp folder');
-    // });
+
+    console.log('Created new release and uploaded assets at: \n' + html_url);
+    rimraf('./tmp', function (rerr) {
+      if (rerr) { throw rerr; }
+      console.log('Succesfully cleaned up tmp folder');
+    });
   });    
 };
 
 var client = github.client(require('./deploy/auth.json').token);
 var ghrepo = client.repo(pkg.repository.name);
-ghrepo.releases(function (err, body) {
-  return body.filter(function (release) {
-    if (release.tag_name === require('./package.json').version) {
-      var ghrelease = client.release(pkg.repository.name, release.id);
-      uploadAssets(ghrelease);
-    }
+ghrepo.release({
+  tag_name: version,
+  draft: false
+}, uploadAssets);
+
+function findRelease () {
+  ghrepo.releases(function (err, body) {
+    return body.filter(function (release) {
+      if (release.tag_name === require('./package.json').version) {
+        var ghrelease = client.release(pkg.repository.name, release.id);
+        uploadAssets(ghrelease);
+      }
+    });
   });
-});
+};
